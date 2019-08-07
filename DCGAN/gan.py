9593b1d58d2de3151cfe01701fe9e5677c5e5f62
Shehtab Zaman <szaman5@titan.cs.binghamton.edu>
2019-08-07 03:12:18 -0400

    changing to grayscale

--- DCGAN/gan.py
+++ DCGAN/gan.py
@@ -38,7 +38,7 @@ batch_size = 16
 image_size = 64
 
 # Number of channels in the training images. For color images this is 3
-nc = 3
+nc = 1
 
 # Size of z latent vector (i.e. size of generator input)
 nz = 100
@@ -71,6 +71,7 @@ dataset = dset.ImageFolder(root=dataroot,
                            transform=transforms.Compose([
                                transforms.Resize(image_size),
                                transforms.CenterCrop(image_size),
+                               transforms.grayscale(),
                                transforms.ToTensor(),
                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                            ]))
